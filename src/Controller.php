1541c065665807c60bc6a4af4ae1ddee8dae03a2
Chris Tenday <ctenday@gmail.com>
2023-09-27 15:29:59 +0100

    data reponse handling with trait

--- src/Controller.php
+++ src/Controller.php
@@ -4,47 +4,8 @@ namespace Rtgroup\HttpRouter;
 
 abstract class Controller
 {
-    private array $headers=array(
-        "Content-Type"=>"application/json"
-    );
+    use DataLoader; /** trait pour gerer le chargement des données en reponde http format json. */
 
     public abstract function captured($url, HttpRequest $httpRequest);
 
-    /**
-     * Reponse http, retourne les données en format json.
-     * @param $key
-     * @param $data
-     * @return void
-     */
-    protected function loadData($key, $data)
-    {
-        $dataResponse[$key]=$data;
-
-        /**
-         * Send response headers.
-         */
-        foreach($this->headers as $key=>$val)
-        {
-            header($key.":".$val);
-        }
-
-        $jsonResponse=json_encode($dataResponse);
-
-        /**
-         * Output http response.
-         */
-        echo $jsonResponse;
-        exit(); /** stop the application */
-    }
-
-    /**
-     * Specifier un header pour la reponse http
-     * @param $key
-     * @param $value
-     * @return void
-     */
-    protected function setResponseHeader($key,$value)
-    {
-        $this->headers[$key]=$value;
-    }
 }
